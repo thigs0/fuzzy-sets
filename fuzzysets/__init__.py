6b786e431237e65ca1d3a3837a0d472818d12ffa
thigs0 <tthiagosantos38@gmail.com>
2025-04-22 22:47:49 -0300

    update init

--- fuzzysets/__init__.py
+++ fuzzysets/__init__.py
@@ -1,16 +1,3 @@
-from fuzzysets.plot import (
-    plot_fuzzy_set,
-    plot_fuzzy_set_operations,
-    plot_tfn,
-    plot_tfn_operations,
-)
-from fuzzysets.sets.base import (
-    alpha_cut,
-    complement,
-    s_norm,
-    t_norm,
-)
-
 from fuzzysets.tfn import TriangularFuzzyNumber as TFN
 from fuzzysets.cauchyfuzzyset import CauchyFuzzySet as CauchyFuzzySet
 from fuzzysets.tnorm import Tnorm as Tnorm
